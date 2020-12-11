6f7ddbe824ba335430eaa2b90fff7336456e5445
Leon Michalak <leonmichalak6@gmail.com>
2020-12-11 19:58:31 +0000

    Updated README to include live build

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,12 @@ import Navbar from "./components/util/navbar/Navbar";
 import Collection from "./pages/collection/Collection";
 import { AuthProvider } from "./utils/AuthContext";
 
-// TODO: CREDIT ICONS
-/* <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */
-
-/* <div>Icons made by <a href="http://www.google.com/" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */
-
-// Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-
-// Icons made by <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-
-/* <div>Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart">smalllikeart</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */
-
-// Icons made by <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-
 export default function App() {
   const routes = (
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/test" exact />
-      <Route path="/test/" />
+      <Route path="/test/:collectionId" exact />
       <Route path="/user/:userId/collections/:collectionId" component={Collection} exact />
       <Route path="*">
         <Redirect to="/" />
