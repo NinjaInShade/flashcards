bf0edbb3780b1bfd1283a813e02fd05b7432a762
Leon Michalak <leonmichalak6@gmail.com>
2021-03-14 17:21:46 +0000

    Delete collection route/controller done

--- backend/controllers/collections.js
+++ backend/controllers/collections.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Flashcard = require("../models/Flashcard");
 
 function postAddCollection(req, res, next) {
   const { name, icon } = req.body;
@@ -10,11 +11,33 @@ function postAddCollection(req, res, next) {
       return user.save();
     })
     .then(() => {
-      return res.status(200).json({ message: "New collectiion successfully added", newCollection: { name, icon } });
+      return res.status(200).json({ message: "New collection successfully added", newCollection: { name, icon } });
+    })
+    .catch((err) => console.log(err));
+}
+
+function deleteCollection(req, res, next) {
+  const collectionId = req.params.collectionId;
+
+  // Delete all flashcards that relate to the collection first
+  Flashcard.deleteMany({ collection_id: collectionId })
+    .then(() => {
+      return User.findById(req.user._id);
+    })
+    .then((user) => {
+      let updatedCollections = req.user.collections.filter((c) => c._id.toString() !== collectionId.toString());
+
+      user.collections = updatedCollections;
+
+      return user.save();
+    })
+    .then(() => {
+      return res.status(200).json({ message: "Collection successfully deleted" });
     })
     .catch((err) => console.log(err));
 }
 
 module.exports = {
   postAddCollection,
+  deleteCollection,
 };
--- backend/routes/collections.js
+++ backend/routes/collections.js
@@ -6,4 +6,7 @@ const collectionsController = require("../controllers/collections");
 // POST ( create a new collection )
 router.post("/add", isAuth, collectionsController.postAddCollection);
 
+// DELETE ( delete a collection )
+router.delete("/:collectionId", isAuth, collectionsController.deleteCollection);
+
 module.exports = router;
